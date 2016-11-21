a57081d41956e029e1db88c0b1ed4146835821b9
Sergey Melentyev <sergey.melentyev@gmail.com>
2016-11-21 19:08:55 +0400

    Create main.m

---
+++ main.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+
+// NSSTRING
+NSString *string = @"";		// create an instance of NSString object with literal syntax
+NSString *string = [NSString stringWithFormat: @"%@", objName];		// dynamic instance creation
